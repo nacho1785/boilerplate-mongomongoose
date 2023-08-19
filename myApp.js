17ebe67c4500087a42706e48e946a7b0e0f4fd31
nacho1785 <jorgeichapperon@gmail.com>
2023-08-19 19:51:53 -0300

    Initial Commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect:', err));
 
 let Person;
 
